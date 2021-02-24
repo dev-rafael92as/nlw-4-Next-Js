d432c196fb9e11f9939ebb2cd5f93e707c1bb554
dev-rafael92as <77762757+dev-rafael92as@users.noreply.github.com>
2021-02-24 02:07:14 -0300

    att on react and next - start work in aplication

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import Head from 'next/head'
+import React from 'react';
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../styles/components/Profile';
 
 export default function Home() {
   return (
     <div className="container">
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
